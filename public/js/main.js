420ad2871395c05389cfbe0eb674ea7c61fdc0ed
Adil <adiladil@hotmail.com>
2022-06-25 14:15:41 +0200

    affichage nom terminé

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+function app(){
+    document.getElementById("imgphoto").style.display= "block";
+    document.getElementById("imgjoconde").style.display= "block";
+    document.getElementById("imgtableau").style.display= "block";
+    
+
+};
+
+window.onload = app;
