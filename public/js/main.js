d937c47a845cdfef1172ad3801ee05a5f5e10bd2
Adil <adiladil@hotmail.com>
2022-06-27 09:09:28 +0200

    suppresion fonction javascript

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
-function app(){
-    document.getElementById("imgphoto").style.display= "block";
-    document.getElementById("imgjoconde").style.display= "block";
-    document.getElementById("imgtableau").style.display= "block";
+// function app(){
+//     document.getElementById("imgphoto").style.display= "block";
+//     document.getElementById("imgjoconde").style.display= "block";
+//     document.getElementById("imgtableau").style.display= "block";
     
 
-};
+// };
 
 window.onload = app;
 
